ce29d6fa19d8e1454b709ad539d4d22b74cf1690
lwh <lwh@forlong.io>
2026-02-19 15:40:24 +0900

    const와 let

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,15 @@
 alert("hi");
 
+/**
+ * const = 재할당 불가능(고정값)
+ * let = 재할당 가능
+ */
 const a = 5;
 const b = 2;
-const myName = "ho";
+let myName = "ho";
 
 console.log(a + b);
+console.log(myName);
+
+myName = "lee";
 console.log(myName);
